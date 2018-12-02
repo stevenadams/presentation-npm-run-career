bb99d02600b3d830fd346c10bf0aa8964c7a6236
Steven Adams <steven.adams@teamwork.com>
2018-12-02 20:19:54 +0000

    Initial commit

---
+++ src/components/Presenters/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <section
+    class="Prsenters"
+  >
+    <ul class="Presenters-list">
+      <li class="Presenters-item">
+        <div class="Presenter">
+          <figure class="Presenter-item">
+            <img :src="stevenImage" class="Presenter-image">
+          </figure>
+          <h1>Steven Adams</h1>
+          <!-- <p>UI Engineer</p> -->
+          <p>@steven_adams</p>
+        </div>
+      </li>
+      <li class="Presenters-item">
+        <div class="Presenter">
+          <figure class="Presenter-item">
+            <img :src="leeImage" class="Presenter-image">
+          </figure>
+          <h1>Lee Campbell</h1>
+          <!-- <p>UI Engineer</p> -->
+          <!-- <p>@steven_adams</p> -->
+        </div>
+      </li>
+    </ul>
+
+  </section>
+</template>
+<style lang="scss" scoped src="./Presenters.scss"></style>
+<script>
+import stevenImage from './img/steven-adams.png'
+import leeImage from './img/lee-campbell.jpg'
+export default {
+  data () {
+    return {
+      stevenImage: stevenImage,
+      leeImage: leeImage
+    }
+  }
+}
+</script>
---
+++ src/components/TitleScreen/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <section
+    class="TitleScreen"
+  >
+    <h1>
+      <slot name="title"></slot>
+    </h1>
+    <h2>
+      Ulster University Jordanstown | 3rd Dec 2018
+    </h2>
+  </section>
+</template>
+<style lang="scss" scoped src="./TitleScreen.scss"/>
